418c6fa324a9d18540e65330ac796c71a7edb146
sdjagtappulsepoint <sdjagtap@pulsepoint.com>
2025-06-05 17:41:59 +0530

    even number list & list creator name

--- Logic.py
+++ Logic.py
@@ -2,3 +2,11 @@ for i in range(1,7):
     if i == 3:
         continue
     print(f"{i} * {i} = {i*i} ")
+
+even_list =[]
+for i in range(0,11):
+    if i % 2 == 0 :
+        even_list.append(i)
+print(even_list)
+
+print (f" Sachin has created an even number list. {even_list}")
